1222158ac5e02d3dc4959332ffacc75020361e32
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-12 21:57:09 -0300

    Update tabs.dart

--- vamos_cozinhar_app_praticando_navegacoes/lib/screens/tabs.dart
+++ vamos_cozinhar_app_praticando_navegacoes/lib/screens/tabs.dart
@@ -13,21 +13,29 @@ class Tabs extends StatefulWidget {
 class _TabsState extends State<Tabs> {
   @override
   Widget build(BuildContext context) {
+    // Lista de Tabs para TabController
+    List<Tab> _tabs = [
+      const Tab(
+        icon: Icon(Icons.category),
+        text: 'Categorias',
+      ),
+      const Tab(
+        icon: Icon(Icons.star),
+        text: 'Favoritos',
+      ),
+    ];
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
+          /*
+          Definindo o Titulo da appbar
+          e chamando a lista de tabs criada acima
+          na tabbar 
+          */
           title: const Text('Vamos Cozinhar?'),
-          bottom: const TabBar(tabs: [
-            Tab(
-              icon: Icon(Icons.category),
-              text: 'Categorias',
-            ),
-            Tab(
-              icon: Icon(Icons.star),
-              text: 'Favoritos',
-            ),
-          ]),
+          bottom: TabBar(tabs: _tabs),
         ),
         body: const TabBarView(
           children: [
